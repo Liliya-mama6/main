1d1c64b84dc879419b48394f3eba503c01727ba8
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-06-30 23:10:42 +0500

    Add files via upload

---
+++ lila.py
@@ -0,0 +1,26 @@
+#1st program
+a=9**0.5*5
+print(a)
+
+#2nd program
+a=True
+if 9.99>9.98 and 1000!=1000.1:
+    a=True
+else:
+    a=False
+print(a)
+
+#3rd program
+a, b=1234, 5678
+a, b=a//10%100, b//10%100
+print(a+b)
+
+#4th program
+a, b=42.13, 13.42
+c=True
+if b*100%100==a//1 or a*100%100==b//1:
+    c=True
+else:
+    c=False
+print(c)
+
